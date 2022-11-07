92a518b8b9fb013efaa385928b8f14644e71a9b9
ibrkorkut07 <http://github.com/ibrkorkut07>
2022-11-07 14:30:13 +0100

    Webdriver setUp

---
+++ src/test/java/Webdriver01.java
@@ -0,0 +1,16 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Webdriver01 {
+    public static void main(String[] args) throws InterruptedException {
+        // Webdriver ayarlamalarini sisteme yapmak icin
+        System.setProperty("Webdriver.chrome.driver", "src/driver/chromedriver.exe");
+        // Simdi bir data turu WebDriver constructor i ChromeDriver olan obje si olusturabiliriz.
+        // Temelde olusturdugumuz obje ChromeDriver in ozelliklerini kullanan bir WebDriver objesi
+        // WebDriver bir interface oldugu ve interface lerin contructor i olmaz ve obje uretilemez
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.amazon.com");
+        Thread.sleep(3000);
+        driver.close();
+    }
+}
